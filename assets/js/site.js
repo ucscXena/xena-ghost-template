d6ea3d97422ffc43f982533538f2453b1d163f74
MillenniumFalconMechanic <mim@clevercanary.com>
2016-05-22 15:50:39 -0400

    Updated font size for root element. Converted JS to be module-based

--- assets/js/site.js
+++ assets/js/site.js
@@ -4,36 +4,70 @@
  * Site functionality.
  */
 
-(function init() {
+(function() {
 
     "use strict";
 
-    // Set up hamburger/menu toggle
-    initHamburger();
-
     /**
-     * Add/remove class on body to toggle visibility of nav menu on mobile. Must explicitly toggle to a "closed"
-     * class (rather than just removing the open class) so that we can animate to the closed class as the final state.
+     * USCS website module definition.
+     *
+     * @returns {Object} Public API of Xena website.
      */
-    function initHamburger() {
+    var UCSC_XENA = (function() {
 
-        var CSS_NAVBAR_OPEN = "navbar-open";
-        var CSS_NAVBAR_CLOSED = "navbar-closed";
+        /**
+         * PUBLIC API
+         */
+        return {
 
-        var $body = $("body");
-        $(".navbar-hamburger").click(function(evt) {
+            // Called on page load to set up components, state, data
+            init: init
+        };
 
-            // Mind you own business!
-            evt.preventDefault();
+        /**
+         * PRIVATES
+         */
 
-            // Menu has been previously toggled - swap out open/closed state
-            if ( $body.hasClass(CSS_NAVBAR_CLOSED) || $body.hasClass(CSS_NAVBAR_OPEN) ) {
-                $body.toggleClass([CSS_NAVBAR_CLOSED, CSS_NAVBAR_OPEN].join(" "));
-            }
-            // Menu has never been toggled - open menu
-            else {
-                $body.addClass(CSS_NAVBAR_OPEN);
-            }
-        });
-    }
+        /**
+         * Set up components, state, data
+         */
+        function init() {
+
+            // Set up hamburger/menu toggle
+            initHamburger();
+
+            // Set up forum
+            // initForum();
+        }
+
+        /**
+         * Add/remove class on body to toggle visibility of nav menu on mobile. Must explicitly toggle to a "closed"
+         * class (rather than just removing the open class) so that we can animate to the closed class as the final state.
+         */
+        function initHamburger() {
+
+            var CSS_NAVBAR_OPEN = "navbar-open";
+            var CSS_NAVBAR_CLOSED = "navbar-closed";
+
+            var $body = $("body");
+            $(".navbar-hamburger").click(function(evt) {
+
+                // Mind you own business!
+                evt.preventDefault();
+
+                // Menu has been previously toggled - swap out open/closed state
+                if ( $body.hasClass(CSS_NAVBAR_CLOSED) || $body.hasClass(CSS_NAVBAR_OPEN) ) {
+                    $body.toggleClass([CSS_NAVBAR_CLOSED, CSS_NAVBAR_OPEN].join(" "));
+                }
+                // Menu has never been toggled - open menu
+                else {
+                    $body.addClass(CSS_NAVBAR_OPEN);
+                }
+            });
+        }
+    })();
+
+    // Kick off initialization of website...
+    UCSC_XENA.init();
+
 })();
