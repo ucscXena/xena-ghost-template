176b205209a3e1d880f4f911cff374b71db27360
Cleto Barbosa <cleto@sjinnovation.com>
2016-05-25 08:06:40 +0530

    Forum threads (#53)
    * latest forum posts
    
    * Latest forum threads added
    
    * implemented mailing list functionality (#52)
    
    * implemented mailing list functionality
    
    * replaced single with double quotes in site.js
    
    * Minor UI updates
    
    * Minor updates
    
    * latest forum posts
    
    * Latest forum threads added
    
    * Minor updates

--- assets/js/site.js
+++ assets/js/site.js
@@ -37,7 +37,7 @@
             initHamburger();
 
             // Set up forum
-            // initForum();
+            initForum();
 
             // Mailchimp form
             initMailchimpForm();
@@ -69,7 +69,11 @@
             });
         }
 
+        /**
+         * Init Mailchimp form
+         */
         function initMailchimpForm() {
+
             window.fnames = new Array();
             window.ftypes = new Array();
             fnames[0] = "EMAIL";
@@ -79,6 +83,53 @@
             fnames[2] = "LNAME";
             ftypes[2] = "text";
         }
+
+        /**
+         * Initiate Forum
+         */
+         function initForum() {
+
+            var url = "https://groups.google.com/forum/feed/ucsc-cancer-genomics-browser/msgs/rss_v2_0.xml?num=20";
+            readRSSFeed(url,parseRSSFeed);
+         }
+
+        /**
+         * Read RSS feed method
+         * @param {string} url - Forum url
+         * @param {function} callback - callback function for parseRSSFeed
+         */
+         function readRSSFeed(url, callback) {
+
+            $.ajax({
+                url: document.location.protocol + "//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=" + encodeURIComponent(url),
+                dataType: "json",
+                success: function(data) {
+                    callback(data.responseData.feed);
+                }
+            });
+         }
+
+         /**
+         * Parse RSS feed method
+         * @param {JSON object} data - latest forum data in JSON format
+         */
+         function parseRSSFeed(data) {
+
+            var entries = data.entries;
+            var html = "";
+            var count = 0;
+
+            $.each(entries, function(i, item) {
+
+                if ( item.title.indexOf("Re:") === -1 && count < 3 ) {
+                    html = html + "<li><a href='"+ item.link +"' target='_blank'>"+ item.title +"</a><span>By "+ item.author +"</span></li>";
+                    count++;
+                }
+            });
+
+            $("#forum-group").html(html);
+         }
+
     })();
 
     // Kick off initialization of website...
