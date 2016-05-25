5eecd5243218ae2aa4cc30b309f2c8af96f8ee2f
anika123 <anika@sjinnovation.com>
2016-05-25 05:48:40 +0530

    implemented mailing list functionality (#52)
    * implemented mailing list functionality
    
    * replaced single with double quotes in site.js
    
    * Minor UI updates

--- assets/js/site.js
+++ assets/js/site.js
@@ -38,6 +38,9 @@
 
             // Set up forum
             // initForum();
+
+            // Mailchimp form
+            initMailchimpForm();
         }
 
         /**
@@ -64,6 +67,17 @@
                     $body.addClass(CSS_NAVBAR_OPEN);
                 }
             });
+        }
+
+        function initMailchimpForm() {
+            window.fnames = new Array();
+            window.ftypes = new Array();
+            fnames[0] = "EMAIL";
+            ftypes[0] = "email";
+            fnames[1] = "FNAME";
+            ftypes[1] = "text";
+            fnames[2] = "LNAME";
+            ftypes[2] = "text";
         }
     })();
 
